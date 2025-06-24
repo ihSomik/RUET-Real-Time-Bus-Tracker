efb5e4754b7efb8f513b4d6ae71a3aa371e56da9
Md. Imdadul Haque Somik <imdadul.haque.somik@gmail.com>
2025-06-24 23:19:00 +0600

    Delete django_project directory

--- django_project/tracker/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Bus(models.Model):
-    number = models.CharField(max_length=10, unique=True)
-    current_lat = models.FloatField()
-    current_lng = models.FloatField()
-
-    def __str__(self):
-        return self.number
--- django_project/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.map_view, name='map'),
-    path('api/bus-locations/', views.bus_locations, name='bus_locations'),
-]
-
